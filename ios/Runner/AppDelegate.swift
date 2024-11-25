bcc53cdfff3dd6815e2dd99029c1ab9949e9ff66
cabrel_sianou <sianou93@gmail.com>
2024-11-25 17:04:02 +0100

    UIKID add for black screenshot

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,45 +1,29 @@
 import UIKit
 import Flutter
 
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    var field = UITextField()
-    var protectionView: UIView? // Vue de protection
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    self.window.makeSecure() // + add this line
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
 
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    override func applicationWillResignActive(_ application: UIApplication) {
-        // Ajouter une vue pour protéger l'écran
-        addProtectionView()
-    }
-
-    override func applicationDidBecomeActive(_ application: UIApplication) {
-        // Retirer la vue de protection
-        removeProtectionView()
-    }
-
-    private func addProtectionView() {
-        // Créer une vue noire qui couvre tout l'écran
-        if protectionView == nil {
-            protectionView = UIView(frame: window?.frame ?? UIScreen.main.bounds)
-            protectionView?.backgroundColor = UIColor.black
-            protectionView?.alpha = 1.0
-        }
-        // Ajouter cette vue à la fenêtre principale
-        if let protectionView = protectionView, let window = window {
-            window.addSubview(protectionView)
-        }
-    }
-
-    private func removeProtectionView() {
-        // Supprimer la vue noire lorsqu'elle n'est plus nécessaire
-        protectionView?.removeFromSuperview()
-        protectionView = nil
+// +  add this section
+extension UIWindow {
+    func makeSecure() {
+        let field = UITextField()
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: field.frame.self.width, height: field.frame.self.height))
+        field.isSecureTextEntry = true
+        self.addSubview(field)
+        self.layer.superlayer?.addSublayer(field.layer)
+        field.layer.sublayers?.last!.addSublayer(self.layer)
+        field.leftView = view
+        field.leftViewMode = .always
     }
 }
